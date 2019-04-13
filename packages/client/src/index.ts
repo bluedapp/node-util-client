f973fbdf7ede4ac6da6652d3a61c7555a443ae6d
jarvis <jiashunming@outlook.com>
2019-04-13 11:01:40 +0800

    feat: 更新 client interface 的描述

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -4,15 +4,17 @@ import CacheIntl from '@blued-core/cache-intl'
 const suggestInstanceCount = 3
 
 export interface ClientIntl {
+  client: Client,
   clean (): void
 }
 
-export default abstract class Client<T extends ClientIntl> {
-  conf: ConfIntl
-
-  cache: CacheIntl
-
-  constructor(protected interval = 1000, protected keepInstanceCount = suggestInstanceCount) {
+export default abstract class Client<T extends ClientIntl = ClientIntl> {
+  constructor(
+    protected conf: ConfIntl,
+    protected cache: CacheIntl,
+    protected interval = 1000,
+    protected keepInstanceCount = suggestInstanceCount
+  ) {
     if (keepInstanceCount < suggestInstanceCount) console.warn(`suggest: keep instance count larger than ${suggestInstanceCount}`)
   }
 
@@ -21,19 +23,17 @@ export default abstract class Client<T extends ClientIntl> {
    * @param key 获取配置所需的标识
    * @param force 是否强制获取最新数据
    */
-  getClient (key: string, force?: boolean): T {
+  getClient (key: string, force?: boolean): Client {
     if (!force && this.cache.has(key)) {
       return this.cache.get(key)
     }
 
     try {
-      const client = this.buildClient(key)
+      const { client, clean } = this.buildClient(key)
       this.cache.set(key, client)
 
       // 在实例保存数量达到上限时清除当前引用的实例
-      setTimeout(() => {
-        client.clean()
-      }, this.interval * this.keepInstanceCount)
+      setTimeout(clean, this.interval * this.keepInstanceCount)
 
       return client
     } catch (e) {
