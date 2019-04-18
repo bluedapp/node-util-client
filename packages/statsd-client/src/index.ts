79286b023e85412df3a871c2996b260f00feedf8
jarvis <jiashunming@outlook.com>
2019-04-18 17:59:01 +0800

    feat: first commit🎉

---
+++ packages/statsd-client/src/index.ts
@@ -0,0 +1,63 @@
+import dgram from 'dgram'
+import os from 'os'
+import Client from '@blued-core/client'
+import { NormalConf } from '@blued-core/normal-conf'
+
+const localIp = os.hostname().replace(/[-.]/g, '_')
+export enum StatsdType {
+  Counting = 'c',
+  Timing = 'ms'
+}
+
+interface StatdClient {
+  client: {
+    timer(path: string, val: number): void,
+    counter(path: string, val: number): void
+  },
+  clean: () => any }
+
+export default class extends Client<StatdClient> {
+  conf: NormalConf
+
+  buildClient (key: string) {
+    const serverSocket = dgram.createSocket('udp4')
+    const { conf, port, group, project } = this.conf.get(key)
+
+    let client = {
+      timer(path: string, val: number) {
+        if (!this.dev) sendMessage(path, val, StatsdType.Timing)
+      },
+      counter(path: string, val: number = 1) {
+        if (!this.dev) sendMessage(path, val, StatsdType.Counting)
+      },
+    }
+
+    return {
+      client,
+      clean () {
+        client = null
+      },
+    }
+
+    function sendMessage (path: string, val: number, type: StatsdType) {
+      const msg = Buffer.from(`${removeDot(group)}.${removeDot(project)}.${localIp}${translatePath(path)}:${val}|${type}`)
+      serverSocket.send(msg, port, conf)
+    }
+  }
+}
+
+/**
+ * 避免变量中存在 . 符号导致 statd 路径增长
+ * @param str 进行转换的字符串
+ */
+function removeDot (str: string) {
+  return str.replace(/\./g, '_')
+}
+
+/**
+ * 将路径转换为 stats 中可以进行分割的字符串 a/b/c -> a.b.c
+ * @param str 进行转换的字符串
+ */
+function translatePath (str: string) {
+  return str.replace(/\//g, '.')
+}
