857b519aee1c235b6d473fd99e8ca0f174aa83c8
jarvis <jiashunming@outlook.com>
2019-04-23 10:56:30 +0800

    refactor: 添加范型的定义

--- packages/raven-client/src/index.ts
+++ packages/raven-client/src/index.ts
@@ -1,13 +1,12 @@
 import Raven from 'raven'
 import Client from '@blued-core/client'
-import { NormalConf } from '@blued-core/normal-conf'
 
-export default class extends Client<typeof Raven> {
-  conf: NormalConf
-
+export default class A extends Client<typeof Raven, string, {
+  qconf: string
+} | string> {
   buildClient (key: string) {
     const conf = this.conf.get(key)
-    Raven.config(!this.dev && conf).install()
+    Raven.config(!this.isLocal && conf).install()
     Raven.disableConsoleAlerts()
     return {
       client: Raven,
