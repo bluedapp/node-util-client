bf9e9856297fb57fe07d7ffbebecd752c124045e
jarvis <jiashunming@outlook.com>
2019-04-24 11:51:22 +0800

    build: upgrade package version

--- packages/raven-client/src/index.ts
+++ packages/raven-client/src/index.ts
@@ -1,9 +1,10 @@
 import Raven from 'raven'
 import Client from '@blued-core/client'
+import { ErrorReportClientIntl } from '@blued-core/client-intl'
 
 export default class RavenClient extends Client<typeof Raven, string, {
   qconf: string
-} | string> {
+} | string> implements ErrorReportClientIntl {
   buildClient (key: string) {
     const conf = this.conf.get(key)
     Raven.config(!this.isLocal && conf).install()
@@ -14,5 +15,3 @@ export default class RavenClient extends Client<typeof Raven, string, {
     }
   }
 }
-
-export type RavenClientBuilder = () => ReturnType<typeof RavenClient.prototype.getClient>
