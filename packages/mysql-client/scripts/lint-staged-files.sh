85ca172e87a2847861527ae0f5a691149cf2feaf
jarvis <jiashunming@outlook.com>
2019-05-18 13:35:24 +0800

    build: remove unuse scripts

--- packages/mysql-client/scripts/lint-staged-files.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git diff --diff-filter=d --cached --name-only | grep -v '^dist/*' | grep -E '\.(js|jsx|ts|tsx)$' | xargs -I % sh -c 'git show ":%" | npx eslint --stdin --stdin-filename "%"'
