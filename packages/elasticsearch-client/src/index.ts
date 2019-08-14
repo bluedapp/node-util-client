434d3db1e77678e8454a4ca492fb4cfd22f78204
bojan <liuboyang276080914@gmail.com>
2019-08-14 15:44:03 +0800

    feat: es-client

--- packages/elasticsearch-client/src/index.ts
+++ packages/elasticsearch-client/src/index.ts
@@ -1,3 +1,24 @@
-export default function main () {
-  return 'Hello World.'
+
+import Client from '@blued-core/client'
+import { Client as BaseClient } from '@elastic/elasticsearch'
+
+export * from '@elastic/elasticsearch'
+
+export default class ElasticsearchClient extends Client<BaseClient, string> {
+  buildClient(key: string) {
+    const host = this.conf.get(key)
+
+    const client = createElasticsearchClient(host)
+
+    return {
+      client,
+      clean () {
+        client.close()
+      },
+    }
+  }
+}
+
+function createElasticsearchClient (host: string) {
+  return new BaseClient({ node: `http://${host}` })
 }
--- packages/elasticsearch-client/test/unit/index.spec.ts
+++ packages/elasticsearch-client/test/unit/index.spec.ts
@@ -1,11 +1,1 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { expect } from 'chai'
-import main from '../../src/index'
 
-describe('normal', () => {
-  it(`function return string`, done => {
-    const res = main()
-    expect(res).to.be.a('string')
-    done()
-  })
-})
