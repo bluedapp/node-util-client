40de2610decd2f70d517074d2fe4d0e098eed3f0
fengcang <fengcang@blued.com>
2020-03-17 11:10:50 +0800

    fix(getclient)

--- packages/grpc-client/src/index.ts
+++ packages/grpc-client/src/index.ts
@@ -30,10 +30,10 @@ export class GrpcReportClient extends Client {
     this.protoPath = protoPath
   }
 
-  getClient(): {
+  getClient(key = 'any', force = false): {
     report<T>(arg: T): any
   } {
-    return super.getClient('any', true)
+    return super.getClient(key, force)
   }
 
   buildClient() {
