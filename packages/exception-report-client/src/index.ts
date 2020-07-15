c04125fb64df966f29eac8ebdcab02232dd0ff41
yaf <lov.2014@gmail.com>
2020-07-15 16:06:53 +0800

    feat: filter

--- packages/exception-report-client/src/index.ts
+++ packages/exception-report-client/src/index.ts
@@ -3,6 +3,9 @@ import Client from '@blued-core/client'
 import { ExceptionReportClientIntl } from '@blued-core/client-intl'
 
 let flag = true
+const EventCache = new Map()
+const LoopSecond = 10
+const ExpireSecond = 60
 
 export default class ExceptionReportClient extends Client<typeof Sentry, string> implements ExceptionReportClientIntl {
   buildClient (key: string) {
@@ -12,9 +15,30 @@ export default class ExceptionReportClient extends Client<typeof Sentry, string>
       Sentry.init({
         dsn: !this.isLocal && conf,
         debug: this.isLocal,
+        beforeSend: (e, hint) => {
+          const error = hint.originalException || e
+          if (EventCache.has(error)) {
+            return EventCache.get(error) <= 0 ? e : null
+          } else {
+            EventCache.set(error, ExpireSecond)
+            return e
+          }
+        },
         ...this.option,
       })
+
+      // 处理缓存错误的有效期
+      setInterval(() => {
+        EventCache.forEach((v, k) => {
+          if (v <= 0) {
+            EventCache.delete(k)
+          } else {
+            EventCache.set(k, v - LoopSecond)
+          }
+        })
+      }, LoopSecond * 1e3)
     }
+
     return {
       client: Sentry,
       clean () { },
