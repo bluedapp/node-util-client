af4da48afe095ab3fb9b66b3ffa53be05183ac24
srvz <srvz@example.com>
2021-01-14 17:34:13 +0800

    feat: healthchecks-ping-client

---
+++ packages/healthchecks-ping-client/README.md
@@ -0,0 +1,38 @@
+## healthchecks-ping-client
+
+
+### 示例
+```
+import { createWrapper, PingClientConfig } from '../src/index'
+
+const config: PingClientConfig = {
+  baseURL: '/healthchecks/host',
+  checks: {
+    test: '/healthchecks/test',
+  }
+}
+
+const sleep = (ms: number) => new Promise(r => setTimeout(r, ms))
+
+const test = async () => {
+  console.log('test start')
+  await sleep(1000)
+  console.log('test end')
+}
+
+(async function main() {
+  try {
+    const wrapper = createWrapper(config)
+    // 第二个参数必须是配置项 checks 中的 key
+    await wrapper(test, 'test')()
+  } catch (e) {
+    console.error('error', e)
+  }
+})()
+
+// 定时任务中使用
+import { scheduleJob } from 'node-schedule'
+
+scheduleJob('1 0 0 * * *', wrapper(test, 'test'))
+```
+
---
+++ packages/healthchecks-ping-client/src/index.ts
@@ -0,0 +1,96 @@
+import { QconfConf, QconfConfItem } from '@blued-core/qconf-conf'
+import axios from 'axios'
+
+export class Ping {
+  private timeout: number
+
+  constructor(
+    public getBaseURL?: () => string,
+    public getCheckId?: () => string,
+    timeout?: number,
+  ) {
+    this.timeout = timeout || 3000
+  }
+
+  async request(signal: string | number, payload?: any) {
+    const baseURL = this.getBaseURL ? this.getBaseURL() : null
+    const checkId = this.getCheckId ? this.getCheckId() : null
+    const path = checkId ? `ping/${checkId}` : null
+    if (!baseURL || !path) {
+      console.warn('[healthchecks-ping-client] ping not avaliable.', { baseURL, path })
+      return
+    }
+    const url = path + (Number.isInteger(signal) || signal ? `/${signal}` : '')
+    try {
+      let data = null
+      if (typeof payload === 'string') {
+        data = payload
+      } else if (typeof payload === 'object') {
+        data = JSON.stringify(payload)
+      } else if (typeof payload === 'number') {
+        data = String(payload)
+      }
+      if (data) {
+        return await axios({ url, baseURL, method: 'post', timeout: this.timeout, data })
+      } else {
+        return await axios({ url, baseURL, method: 'get', timeout: this.timeout })
+      }
+    } catch (e) {
+      console.warn(`[healthchecks-ping-client] Ping ${url} error =>`, e.stack)
+    }
+  }
+
+  ping(payload?: any) {
+    return this.request('', payload)
+  }
+
+  start(payload?: any) {
+    return this.request('start', payload)
+  }
+
+  fail(payload?: any) {
+    return this.request('fail', payload)
+  }
+
+  exit(status: string | number, payload?: any) {
+    return this.request(status, payload)
+  }
+}
+
+export interface PingClientConfig {
+  baseURL: QconfConfItem
+  checks: Record<string, QconfConfItem>
+}
+
+export function createPingClient({
+  baseURL,
+  checks,
+}: PingClientConfig) {
+  const qconfs = new QconfConf(checks)
+  const qhosts = new QconfConf({ baseURL })
+  const clients: Record<string, Ping> = {}
+  if (baseURL) {
+    for (const key of Object.keys(checks)) {
+      clients[key] = new Ping(() => qhosts.get('baseURL'), () => qconfs.get(key))
+    }
+  } else {
+    for (const key of Object.keys(checks)) {
+      clients[key] = new Ping()
+    }
+    console.warn('[healthchecks-ping-client] baseURL not found.')
+  }
+  return clients
+}
+
+export function createWrapper(config: PingClientConfig) {
+  const pingClient = createPingClient(config)
+  return (func: Function, name: string) => async (): Promise<void> => {
+    try {
+      await pingClient[name].start()
+      await func()
+      await pingClient[name].ping()
+    } catch (e) {
+      await pingClient[name].fail({ code: e.code || 0, message: e.message, stack: e.stack })
+    }
+  }
+}
